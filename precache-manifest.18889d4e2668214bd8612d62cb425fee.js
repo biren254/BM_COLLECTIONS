996baed03505c5158e5c5ab45be8c6e0105edfe3
Birendra <biren.moharana01@gmail.com>
2022-09-07 19:42:53 +0530

    Updates

---
+++ precache-manifest.18889d4e2668214bd8612d62cb425fee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "edb508d6badc91b3da7d",
+    "url": "/BM_COLLECTIONS/static/css/main.984f3bdf.chunk.css"
+  },
+  {
+    "revision": "edb508d6badc91b3da7d",
+    "url": "/BM_COLLECTIONS/static/js/main.edb508d6.chunk.js"
+  },
+  {
+    "revision": "d7fe49f3b3a8911f950e",
+    "url": "/BM_COLLECTIONS/static/css/1.d1ffde31.chunk.css"
+  },
+  {
+    "revision": "d7fe49f3b3a8911f950e",
+    "url": "/BM_COLLECTIONS/static/js/1.d7fe49f3.chunk.js"
+  },
+  {
+    "revision": "e7c39314ab10923e48a7",
+    "url": "/BM_COLLECTIONS/static/js/runtime~main.e7c39314.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/BM_COLLECTIONS/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/BM_COLLECTIONS/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/BM_COLLECTIONS/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/BM_COLLECTIONS/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/BM_COLLECTIONS/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "91a79528c43448ba3131306152644def",
+    "url": "/BM_COLLECTIONS/index.html"
+  }
+];
